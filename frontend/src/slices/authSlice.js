38bf824099d4cdf46ee4b16b16bf2ff47ff45272
Sreehari B S <sreeharisreeayilam@gmail.com>
2023-12-13 10:40:56 +0530

    removd the tokens on logout

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -5,7 +5,7 @@ const initialState = {
     ? JSON.parse(localStorage.getItem("userInfo"))
     : null,
 };
-  
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -17,6 +17,8 @@ const authSlice = createSlice({
     logout: (state, action) => {
       state.userInfo = null;
       localStorage.removeItem("userInfo");
+      localStorage.removeItem("userAccessToken");
+      localStorage.removeItem("userRefreshToken");
     },
   },
 });
