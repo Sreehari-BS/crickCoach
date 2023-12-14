c4ab38e34f2f8a5f69274bdd2dc3816655be3a73
Sreehari B S <sreeharisreeayilam@gmail.com>
2023-12-14 10:52:24 +0530

    changed the newSocket to render url

--- frontend/src/components/CoachConnect.jsx
+++ frontend/src/components/CoachConnect.jsx
@@ -25,7 +25,7 @@ import {
 } from "@mui/joy";
 import { toast } from "react-toastify";
 import Loader from "./Loader";
-import CoachHeader from "./CoachHeader"
+import CoachHeader from "./CoachHeader";
 import CoachHomeSidebar from "./CoachHomeSidebar";
 import {
   useFindUserMutation,
@@ -118,7 +118,7 @@ const CoachHome = () => {
   }, [chat]);
 
   useEffect(() => {
-    const newSocket = io("http://localhost:7000");
+    const newSocket = io("https://crickcoach.onrender.com");
 
     newSocket.on("connect", () => {
       setSocket(newSocket);
@@ -155,261 +155,263 @@ const CoachHome = () => {
 
   return (
     <>
-    <CoachHeader />
-    <Box marginTop={8} sx={{ backgroundColor: "#FFF1EE", height: "91vh" }}>
-      <Box
-        display="flex"
-        justifyContent="space-between"
-        maxWidth="95%"
-        mx="auto"
-        paddingTop={"1.5rem"}
-      >
-        <CoachHomeSidebar />
-        <Paper elevation={10} style={{ width: "75%", borderRadius: "20px" }}>
-          <Box p={2}>
-            <Typography
-              level="h2"
-              align="center"
-              fontFamily={"monospace"}
-              gutterBottom
-            >
-              Connect with Customers
-            </Typography>
-            <hr />
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-evenly",
-              }}
-            >
-              <Paper
-                elevation={10}
+      <CoachHeader />
+      <Box marginTop={8} sx={{ backgroundColor: "#FFF1EE", height: "91vh" }}>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          maxWidth="95%"
+          mx="auto"
+          paddingTop={"1.5rem"}
+        >
+          <CoachHomeSidebar />
+          <Paper elevation={10} style={{ width: "75%", borderRadius: "20px" }}>
+            <Box p={2}>
+              <Typography
+                level="h2"
+                align="center"
+                fontFamily={"monospace"}
+                gutterBottom
+              >
+                Connect with Customers
+              </Typography>
+              <hr />
+              <Box
                 sx={{
-                  width: "20%",
-                  height: "550px",
-                  backgroundColor: "#F6AF9A",
-                  borderRadius: "20px",
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "space-evenly",
                 }}
               >
-                <Typography
-                  className="text-center"
-                  sx={{ m: 2, fontFamily: "monospace" }}
-                  level="h4"
-                >
-                  Chats
-                </Typography>
-                <hr style={{ width: "70%", margin: "auto", color: "Black" }} />
-                <Box
+                <Paper
+                  elevation={10}
                   sx={{
-                    height: "460px",
-                    overflow: "hidden",
+                    width: "20%",
+                    height: "550px",
+                    backgroundColor: "#F6AF9A",
+                    borderRadius: "20px",
                   }}
                 >
+                  <Typography
+                    className="text-center"
+                    sx={{ m: 2, fontFamily: "monospace" }}
+                    level="h4"
+                  >
+                    Chats
+                  </Typography>
+                  <hr
+                    style={{ width: "70%", margin: "auto", color: "Black" }}
+                  />
                   <Box
                     sx={{
-                      height: "100%",
-                      overflowY: "scroll",
-                      scrollbarWidth: "none",
-                      msOverflowStyle: "none",
-                      "&::-webkit-scrollbar": {
-                        display: "none",
-                      },
+                      height: "460px",
+                      overflow: "hidden",
                     }}
                   >
-                    {users &&
-                      users.map((user, index) => (
+                    <Box
+                      sx={{
+                        height: "100%",
+                        overflowY: "scroll",
+                        scrollbarWidth: "none",
+                        msOverflowStyle: "none",
+                        "&::-webkit-scrollbar": {
+                          display: "none",
+                        },
+                      }}
+                    >
+                      {users &&
+                        users.map((user, index) => (
+                          <Box
+                            sx={{
+                              display: "flex",
+                              alignItems: "center",
+                              justifyContent: "center",
+                              m: 2,
+                              height: "50px",
+                              backgroundColor: "#CB5839",
+                              borderRadius: "20px",
+                              cursor: "pointer",
+                            }}
+                            key={index}
+                            onClick={() => {
+                              setUser(user);
+                              chatInfo(user._id, coachID);
+                            }}
+                          >
+                            <Avatar
+                              src={user.profileImage}
+                              alt={user.profileImage}
+                            />
+                            <Typography
+                              level="h5"
+                              ml={1}
+                              sx={{ color: "white", fontFamily: "monospace" }}
+                            >
+                              <b>{user.name}</b>
+                            </Typography>
+                          </Box>
+                        ))}
+                      {isLoading && <Loader />}
+                    </Box>
+                  </Box>
+                </Paper>
+
+                <Paper
+                  elevation={10}
+                  sx={{
+                    backgroundColor: "#DCA393",
+                    width: "78%",
+                    height: "550px",
+                    borderRadius: "20px",
+                  }}
+                >
+                  {user ? (
+                    <>
+                      <Box
+                        sx={{
+                          height: "550px",
+                          display: "flex",
+                          flexDirection: "column",
+                          justifyContent: "space-between",
+                        }}
+                      >
                         <Box
                           sx={{
                             display: "flex",
                             alignItems: "center",
                             justifyContent: "center",
-                            m: 2,
-                            height: "50px",
-                            backgroundColor: "#CB5839",
-                            borderRadius: "20px",
-                            cursor: "pointer",
-                          }}
-                          key={index}
-                          onClick={() => {
-                            setUser(user);
-                            chatInfo(user._id, coachID);
                           }}
                         >
                           <Avatar
-                            src={user.profileImage}
-                            alt={user.profileImage}
+                            alt={user && user.profileImage}
+                            src={user && user.profileImage}
                           />
                           <Typography
-                            level="h5"
-                            ml={1}
-                            sx={{ color: "white", fontFamily: "monospace" }}
+                            level="h4"
+                            m={2}
+                            sx={{ color: "#CB5839", fontFamily: "monospace" }}
                           >
-                            <b>{user.name}</b>
+                            <b>{user && user.name}</b>
                           </Typography>
                         </Box>
-                      ))}
-                    {isLoading && <Loader />}
-                  </Box>
-                </Box>
-              </Paper>
-
-              <Paper
-                elevation={10}
-                sx={{
-                  backgroundColor: "#DCA393",
-                  width: "78%",
-                  height: "550px",
-                  borderRadius: "20px",
-                }}
-              >
-                {user ? (
-                  <>
-                    <Box
-                      sx={{
-                        height: "550px",
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "space-between",
-                      }}
-                    >
-                      <Box
-                        sx={{
-                          display: "flex",
-                          alignItems: "center",
-                          justifyContent: "center",
-                        }}
-                      >
-                        <Avatar
-                          alt={user && user.profileImage}
-                          src={user && user.profileImage}
+                        <hr
+                          style={{
+                            color: "black",
+                            width: "60%",
+                            margin: "auto",
+                            marginTop: 0,
+                          }}
                         />
-                        <Typography
-                          level="h4"
-                          m={2}
-                          sx={{ color: "#CB5839", fontFamily: "monospace" }}
-                        >
-                          <b>{user && user.name}</b>
-                        </Typography>
-                      </Box>
-                      <hr
-                        style={{
-                          color: "black",
-                          width: "60%",
-                          margin: "auto",
-                          marginTop: 0,
-                        }}
-                      />
-                      <Box
-                        sx={{
-                          height: "70%",
-                          marginBottom: 2,
-                          display: "flex",
-                          flexDirection: "column-reverse",
-                          overflowY: "scroll",
-                          scrollbarWidth: "none",
-                          msOverflowStyle: "none",
-                          "&::-webkit-scrollbar": {
-                            display: "none",
-                          },
-                        }}
-                      >
                         <Box
                           sx={{
+                            height: "70%",
+                            marginBottom: 2,
                             display: "flex",
-                            flexDirection: "column",
+                            flexDirection: "column-reverse",
+                            overflowY: "scroll",
+                            scrollbarWidth: "none",
+                            msOverflowStyle: "none",
+                            "&::-webkit-scrollbar": {
+                              display: "none",
+                            },
                           }}
                         >
-                          {messages &&
-                            messages.map((message, index) => (
-                              <Box
-                                key={index}
-                                sx={{
-                                  display: "inline-block",
-                                  backgroundColor:
-                                    message.senderId === coachID
-                                      ? "coral"
-                                      : "#1E7B7C",
-                                  color: "white",
-                                  fontFamily: "monospace",
-                                  padding: 2,
-                                  borderRadius: "10px",
-                                  margin: 1,
-                                  marginRight: 20,
-                                  marginLeft: 20,
-                                  alignSelf:
-                                    message.senderId === coachID
-                                      ? "flex-end"
-                                      : "flex-start",
-                                }}
-                              >
-                                <span>{message.text}</span>
-                                <br />
-                                <span
-                                  style={{
-                                    fontSize: 10,
-                                    color: "black",
-                                    fontFamily: "sans-serif",
-                                    textAlign:
+                          <Box
+                            sx={{
+                              display: "flex",
+                              flexDirection: "column",
+                            }}
+                          >
+                            {messages &&
+                              messages.map((message, index) => (
+                                <Box
+                                  key={index}
+                                  sx={{
+                                    display: "inline-block",
+                                    backgroundColor:
                                       message.senderId === coachID
-                                        ? "right"
-                                        : "left",
-                                    display: "block",
+                                        ? "coral"
+                                        : "#1E7B7C",
+                                    color: "white",
+                                    fontFamily: "monospace",
+                                    padding: 2,
+                                    borderRadius: "10px",
+                                    margin: 1,
+                                    marginRight: 20,
+                                    marginLeft: 20,
+                                    alignSelf:
+                                      message.senderId === coachID
+                                        ? "flex-end"
+                                        : "flex-start",
                                   }}
                                 >
-                                  {format(message.createdAt)}
-                                </span>
-                              </Box>
-                            ))}
+                                  <span>{message.text}</span>
+                                  <br />
+                                  <span
+                                    style={{
+                                      fontSize: 10,
+                                      color: "black",
+                                      fontFamily: "sans-serif",
+                                      textAlign:
+                                        message.senderId === coachID
+                                          ? "right"
+                                          : "left",
+                                      display: "block",
+                                    }}
+                                  >
+                                    {format(message.createdAt)}
+                                  </span>
+                                </Box>
+                              ))}
+                          </Box>
+                        </Box>
+                        <Box
+                          sx={{
+                            width: "70%",
+                            margin: "auto",
+                            marginBottom: 2,
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "space-between",
+                          }}
+                        >
+                          <InputEmoji
+                            fontFamily="monospace"
+                            placeholder="Type message..."
+                            theme="auto"
+                            keepOpened="true"
+                            value={text}
+                            onChange={(value) => setText(value)}
+                          />
+                          <Button
+                            onClick={addMessage}
+                            sx={{ backgroundColor: "coral", color: "white" }}
+                          >
+                            <b>Send</b>
+                          </Button>
                         </Box>
                       </Box>
-                      <Box
-                        sx={{
-                          width: "70%",
-                          margin: "auto",
-                          marginBottom: 2,
-                          display: "flex",
-                          alignItems: "center",
-                          justifyContent: "space-between",
-                        }}
-                      >
-                        <InputEmoji
-                          fontFamily="monospace"
-                          placeholder="Type message..."
-                          theme="auto"
-                          keepOpened="true"
-                          value={text}
-                          onChange={(value) => setText(value)}
-                        />
-                        <Button
-                          onClick={addMessage}
-                          sx={{ backgroundColor: "coral", color: "white" }}
-                        >
-                          <b>Send</b>
-                        </Button>
-                      </Box>
-                    </Box>
-                  </>
-                ) : (
-                  <Typography
-                    level="h3"
-                    sx={{
-                      display: "flex",
-                      justifyContent: "center",
-                      mt: "230px",
-                      fontFamily: "monospace",
-                      color: "white",
-                    }}
-                  >
-                    Click on the customers for getting started...
-                  </Typography>
-                )}
-              </Paper>
+                    </>
+                  ) : (
+                    <Typography
+                      level="h3"
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        mt: "230px",
+                        fontFamily: "monospace",
+                        color: "white",
+                      }}
+                    >
+                      Click on the customers for getting started...
+                    </Typography>
+                  )}
+                </Paper>
+              </Box>
             </Box>
-          </Box>
-        </Paper>
+          </Paper>
+        </Box>
       </Box>
-    </Box>
     </>
   );
 };
